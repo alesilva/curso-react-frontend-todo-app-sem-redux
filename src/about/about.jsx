53252be00ed39255e3ea5d79730d3b5873b4fcd8
Alexandre Ribeiro da Silva <alribeiro@uolinc.com>
2021-07-13 20:35:31 -0300

    FrontEnd TodoApp sem Redux

---
+++ src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PagHeader from '../template/pagHeader'
+
+export default props => (
+    <div>
+        <PagHeader name='Sobre' small='Nós' />
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Nossa missão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
